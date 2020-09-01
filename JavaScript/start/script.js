b3ccc5afbcd5527d3f2e19f5aec6d28c6237f009
Raven-Lee <devil.li.53@gmail.com>
2020-09-01 08:59:27 +0800

    V3

---
+++ JavaScript/start/script.js
@@ -0,0 +1,14 @@
+
+
+function createdParagraph(){
+	let para = document.createElement("p");
+	para.textContent = '你点了这个按钮！';
+	document.body.appendChild(para)
+}
+
+const buttons = document.querySelectorAll('button');
+
+for(let i = 0; i < buttons.length; i++){
+	buttons[i].addEventListener('click',createdParagraph);
+}
+
