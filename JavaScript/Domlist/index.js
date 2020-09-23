6b9d32b652787e3a8addc0fd0b5373c2785d4d56
Raven-Lee <devil.li.53@gmail.com>
2020-09-24 02:11:50 +0800

    v0.2

--- JavaScript/Domlist/index.js
+++ JavaScript/Domlist/index.js
@@ -1,30 +1,142 @@
 window.onload = function () {
   let oTab = document.getElementById("tab");
   let aRows = oTab.tBodies[0].rows;
-  var oSerchButton = document.getElementById('serchBtn');
+  var oSerchButton = document.getElementById("serchBtn");
+  var id = aRows.length + 1;
+  var oUser = document.getElementById("userName");
+  var oCreator = document.getElementById("creator");
+  var oCreate = document.getElementById("create");
+  var oNewContainer = document.getElementById("newContainer");
+  var oNewContainerTitle = document.getElementById("newContainerTitle");
+  var oNewContainer_back = document.getElementById("newContainer-background");
   //添加操作栏
   for (let index = 0; index < aRows.length; index++) {
     var oTd = document.createElement("td");
     oTd.innerHTML =
-      '<input type="submit" name="" id="check" value=""><input type="submit" name="" id="edit" value=""><input type="submit" name="" id="del" value="">';
+      '<input type="submit" name="" id="check" value="查看"><input type="submit" name="" id="edit" value="编辑"><input type="submit" name="" id="del" value="删除">';
 
     aRows[index].appendChild(oTd);
   }
-  console.log('操作按钮添加完毕');
 
   //检索
-  
-  console.log(oSerchButton);
-
   oSerchButton.onclick = function () {
     var oSerchText = document.getElementById("serchText");
-
-    for (let index = 0; index < aRows.length; index++) {
-      var content = aRows[index].td[1];
-      console.log(content);
-      if (oSerchText.value == content.value) {
-        content.parentNode.style.backgoundColor = "#007acc";
+    for (let i = 0; i < aRows.length; i++) {
+      if (aRows[i].cells[1].innerHTML.search(oSerchText.value) != -1) {
+        for (let index = 0; index < aRows.length; index++) {
+          aRows[index].style.display = "none";
+        }
+        aRows[i].style.display = "";
       }
     }
   };
+
+  //操作
+  var aBtn = document.getElementsByTagName("input");
+  for (let index = 0; index < aBtn.length; index++) {
+    const element = aBtn[index];
+    //删除当前行
+    if (element.id == "del") {
+      element.onclick = function () {
+        element.parentNode.parentNode.parentNode.removeChild(
+          element.parentNode.parentNode
+        );
+      };
+    }
+    //查看当前行
+    else if (element.id == "check") {
+      //
+      element.onclick = function name(params) {
+        console.log(element.parentNode.parentNode);
+        oNewContainerTitle.innerHTML = "查看用户";
+        oUser.value = element.parentNode.parentNode.cells[1].innerHTML;
+        oCreator.value = element.parentNode.parentNode.cells[2].innerHTML;
+        oNewContainer_back.style.display = "block";
+        oNewContainer.style.display = "block";
+      };
+    }
+    //编辑当前行
+    else if (element.id == "edit") {
+      //
+    }
+  }
+  //新建
+  oCreate.onclick = function () {
+    oNewContainerTitle.innerHTML = "新建用户";
+    oUser.value = "";
+    oCreator.value = "";
+    oNewContainer_back.style.display = "block";
+    oNewContainer.style.display = "block";
+  };
+  //新建提交
+  //to do 新建的数据的操作选项，无法触发
+  var oSubmit = document.getElementById("newSubmit");
+  oSubmit.onclick = function () {
+    var createdDate = new Date();
+    var oTr = document.createElement("tr");
+
+    var oTd = document.createElement("td");
+    oTd.innerHTML = id++;
+    oTr.appendChild(oTd);
+
+    var oTd = document.createElement("td");
+    oTd.innerHTML = oUser.value;
+    oTr.appendChild(oTd);
+
+    var oTd = document.createElement("td");
+    oTd.innerHTML = oCreator.value;
+    oTr.appendChild(oTd);
+
+    var oTd = document.createElement("td");
+    oTd.innerHTML = newDate();
+    oTr.appendChild(oTd);
+
+    var oTd = document.createElement("td");
+    oTd.innerHTML =
+      '<input type="submit" name="" id="check" value="查看"><input type="submit" name="" id="edit" value="编辑"><input type="submit" name="" id="del" value="删除">';
+    oTr.appendChild(oTd);
+
+    oTab.tBodies[0].appendChild(oTr);
+    oNewContainer.style.display = "none";
+    oNewContainer.style.display = "none";
+  };
+  //新建窗口-返回
+  var oBack = document.getElementById("back");
+  oBack.onclick = function () {
+    oNewContainer.style.display = "none";
+    oNewContainer_back.style.display = "none";
+  };
 };
+
+//返回日期字符串
+function newDate() {
+  var myDate = new Date();
+  var sMonth =
+    myDate.getMonth() > 8
+      ? myDate.getMonth() + 1
+      : "0" + (myDate.getMonth() + 1);
+  var sDate = myDate.getDate() > 9 ? myDate.getDate() : "0" + myDate.getDate();
+  var sHour =
+    myDate.getHours() > 9 ? myDate.getHours() : "0" + myDate.getHours();
+  var sMinute =
+    myDate.getMinutes() > 9 ? myDate.getMinutes() : "0" + myDate.getMinutes();
+  var sSecond =
+    myDate.getSeconds() > 9 ? myDate.getSeconds() : "0" + myDate.getSeconds();
+  return (
+    myDate.getFullYear() +
+    "-" +
+    sMonth +
+    "-" +
+    sDate +
+    " " +
+    sHour +
+    ":" +
+    sMinute +
+    ":" +
+    sSecond
+  );
+}
+
+function check(params) {
+  var oNewContainer = document.getElementById("newContainer");
+}
