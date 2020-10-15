0134180f010e0471ed542a8aaa20a374ed58ede4
Raven-Lee <devil.li.53@gmail.com>
2020-10-15 22:42:10 +0800

    v0.51

--- JavaScript/Movement/侧浮窗居中运动/index.js
+++ JavaScript/Movement/侧浮窗居中运动/index.js
@@ -2,18 +2,31 @@ window.onscroll = function () {
   var oDiv = document.getElementById("div1");
   var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
 
-
-  oDiv.style.top = document.documentElement.clientHeight - oDiv.offsetHeight + scrollTop + 'px';
-
-  console.log(oDiv.offsetHeight);
+  // oDiv.style.top =
+  //   document.documentElement.clientHeight -
+  //   oDiv.offsetHeight +
+  //   scrollTop +
+  //   "px";
+  console.log('目标高度'+(document.documentElement.clientHeight - oDiv.offsetHeight + scrollTop));
+  console.log('物体当前高度'+oDiv.offsetTop);
+  console.log('目标高度'+document.documentElement.clientHeight - oDiv.offsetHeight + scrollTop);
+  move(document.documentElement.clientHeight - oDiv.offsetHeight + scrollTop);
+  
 };
 
+var timer = null;
+function move(iTartget) {
+  var oDiv = document.getElementById("div1");
 
+  clearInterval(timer);
+  timer = setInterval(() => {
+    var speed = (iTartget - oDiv.offsetTop) / 20;
+    speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
 
-var timer= null
-function move(iTartget){
-     clearInterval(timer)
-     timer = setInterval(() => {
-         
-     }, 30);
+    if (oDiv.offsetTop == iTartget) {
+      clearInterval(timer);
+    } else {
+      oDiv.style.top = oDiv.offsetTop + speed + "px";
+    }
+  }, 30);
 }
